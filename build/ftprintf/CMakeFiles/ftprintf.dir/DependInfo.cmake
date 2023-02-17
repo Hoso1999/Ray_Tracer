cfcb50d333b6b4e9420a9fa3398f7737a91a2118
Hovsep Hayrapetyan <hoso.hayrapetyan@gmail.com>
2023-02-17 23:13:38 +0400

    fixed cmake

---
+++ build/ftprintf/CMakeFiles/ftprintf.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_digit_functions.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_digit_functions.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_digit_functions.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_events.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_events.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_events.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_printf.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_printf.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_printf.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_prints.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_prints.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_prints.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_type_digit.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_digit.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_digit.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_type_hex.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_hex.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_hex.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_type_others.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_others.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_others.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_type_strings.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_strings.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_strings.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_type_udigit.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_udigit.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_udigit.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/ft_type_upperhex.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_upperhex.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/ft_type_upperhex.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/utils.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/utils.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/utils.c.o.d"
+  "/iSCSI/miniRT/lib/ftprintf/src/utils2.c" "build/ftprintf/CMakeFiles/ftprintf.dir/src/utils2.c.o" "gcc" "build/ftprintf/CMakeFiles/ftprintf.dir/src/utils2.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
